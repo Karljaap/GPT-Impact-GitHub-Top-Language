aaf9ac6534baae4f204b2f386fd283ca5dc14b3d
Jesus2023A <144987049+Jesus2023A@users.noreply.github.com>
2026-03-17 18:15:24 -0500

    Final

--- run_paper.py
+++ run_paper.py
@@ -60,6 +60,8 @@ TABLE_LABELS = {
         "tab:tabla3",
     os.path.join(BASE_DIR, "output", "tables", "gpt_impact_github_DataScience_controls.tex"):
         "tab:tabla4",
+    os.path.join(BASE_DIR, "output", "tables", "placebo_test.tex"):
+        "tab:tabla5",
 }
 
 for fpath, label in TABLE_LABELS.items():
